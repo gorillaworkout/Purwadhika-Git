802b73e23552c635c8b0cde59e3c57dc535fcdaf
Bayu Darmawan <darmawanbayu1@gmail.com>
2020-07-24 14:31:58 +0700

    adding testing.js

---
+++ testing.js
@@ -0,0 +1,4 @@
+/* buka terminal di repository yang ingin di tambahkan init
+git init -> buat install initialize di dalam folder
+git status - > check data apakah udh di add atau blm.area-komputer
+*/
